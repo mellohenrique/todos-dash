73ecc7b09429a921104ccd93fd02f19809f300c8
Henrique Assuncao <henrique.mello1992@gmail.com>
2020-03-24 13:51:11 -0300

    Adiciona quebra de linha

--- modules/select_numeric_vector.R
+++ modules/select_numeric_vector.R
@@ -10,22 +10,22 @@ select_numeric_vector <- function(id) {
   tagList(
   textInput(
     ns("complem_uniao"),
-    "Complementação da União (valores anuais separados por vírgula, ponto sendo símbolo de decimal",
+    HTML("complementação da União para fundos estaduais, modelo atual<br/>(Valores anuais separados por vírgula, ponto sendo símbolo de decimal)"),
     "0.1, 0.12"
   ),
   textInput(
     ns("complem_uniao_vaat"),
-    "Complementação da União na segunda etapa do modelo híbrido (valores anuais separados por vírgula, ponto sendo símbolo de decimal",
+    HTML("Complementação da União no modelo VAAT<br/>(Valores anuais separados por vírgula, ponto sendo símbolo de decimal"),
     "0.05, 0.06"
   ),
   textInput(
     ns("crescimento_economico"),
-    "Valor do crescimento econômico esperado no modelo a cada ano (valores anuais separados por vírgula, ponto sendo símbolo de decimal",
+    HTML("Valor do crescimento econômico esperado no modelo a cada ano<br/>(Valores anuais separados por vírgula, ponto sendo símbolo de decimal"),
     "0, 0.02"
   ),
   textInput(
     ns("crescimento_demografico"),
-    "Valor do crescimento demográfico da população de alunos esperado no modelo a cada ano  (valores anuais separados por vírgula, ponto sendo símbolo de decimal",
+    HTML("Valor do crescimento demográfico da população de alunos esperado no modelo a cada ano<br/>(Valores anuais separados por vírgula, ponto sendo símbolo de decimal)"),
     "0, -0.02"
   )
   )
